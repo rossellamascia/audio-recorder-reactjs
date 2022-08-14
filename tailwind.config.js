16128bcc6e094d4406bc160946c59eb9635a6766
Rossella Mascia <rossella.mascia@neosyn.it>
2022-08-14 14:37:18 +0200

    add tailwind + heroicon

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    colors: {
+      "white": "#ffff",
+      "primary": "#662400",
+      "secondary": "#B33F00",
+      "tertiary": "#FF6B1A",
+    },
+    extend: {},
+  },
+  plugins: [],
+}
